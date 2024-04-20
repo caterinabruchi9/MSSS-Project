c924414dd090a95546c02d0b609c9d829739d283
MarcoBolo001 <143323325+MarcoBolo001@users.noreply.github.com>
2024-04-20 21:54:21 +0200

    MainActivity & WifiScanner

--- Fingerprinting Application/app/src/main/java/it/unipi/dii/fingerprintingapplication/MainActivity.kt
+++ Fingerprinting Application/app/src/main/java/it/unipi/dii/fingerprintingapplication/MainActivity.kt
@@ -1,20 +1,84 @@
 package it.unipi.dii.fingerprintingapplication
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import it.unipi.dii.fingerprintingapplication.R
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val LOCATION_PERMISSION_REQUEST_CODE = 100
+    }
+
+    private lateinit var wifiScanner: WifiScanner
+    private lateinit var textViewResults: TextView
+    private lateinit var editTextIdArea: EditText
+    private lateinit var editTextCoordinates: EditText
+    private lateinit var buttonScan: Button
+    private lateinit var textViewStatus: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        wifiScanner = WifiScanner(this)
+        textViewResults = findViewById(R.id.textViewResults)
+        textViewStatus = findViewById(R.id.textViewStatus)
+        editTextIdArea= findViewById(R.id.editTextIdArea)
+        editTextCoordinates = findViewById(R.id.editTextCoordinates)
+        buttonScan = findViewById(R.id.buttonScan)
+
+        wifiScanner.wifiScanResults.observe(this, Observer { results ->
+            val resultText = results.joinToString("\n") { "${it.SSID}, ${it.level}dBm, ${it.frequency}MHz" }
+            textViewResults.text = resultText
+            textViewStatus.text = "Scan completato"
+            buttonScan.isEnabled = true
+        })
+
+        buttonScan.setOnClickListener {
+            if (checkLocationPermission()) {
+                textViewStatus.text = "Scan in corso..."
+                buttonScan.isEnabled = false
+               // wifiScanner.startScan()
+                wifiScanner.toggleWifi()
+                wifiScanner.startScanDelayed()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    LOCATION_PERMISSION_REQUEST_CODE
+                )
+            }
         }
+    }
+
+    private fun checkLocationPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                textViewStatus.text = "Scan in corso..."
+                buttonScan.isEnabled = false
+                // Permission was granted, resume the scan.
+                wifiScanner.startScan()
+            } else {
+                // Permission denied, disable the functionality that depends on this permission.
+                textViewResults.text = "Permission denied. Cannot perform WiFi scan."
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        wifiScanner.unregisterReceiver() // Unregister receiver to avoid memory leaks
     }
 }
--- Fingerprinting Application/app/src/main/java/it/unipi/dii/fingerprintingapplication/WifiScanner.kt
+++ Fingerprinting Application/app/src/main/java/it/unipi/dii/fingerprintingapplication/WifiScanner.kt
@@ -1,4 +1,68 @@
 package it.unipi.dii.fingerprintingapplication
 
-class WifiScanner {
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.wifi.ScanResult
+import android.net.wifi.WifiManager
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+import android.os.Handler
+import android.os.Looper
+
+class WifiScanner(private val context: Context) {
+    private var wifiManager: WifiManager = context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+    private val _wifiScanResults = MutableLiveData<List<ScanResult>>()
+    val wifiScanResults: LiveData<List<ScanResult>> = _wifiScanResults
+
+    private val wifiScanReceiver = object : BroadcastReceiver() {
+        override fun onReceive(c: Context, intent: Intent) {
+            if (WifiManager.SCAN_RESULTS_AVAILABLE_ACTION == intent.action) {
+                _wifiScanResults.value = wifiManager.scanResults
+            }
+        }
+    }
+
+    init {
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+        context.registerReceiver(wifiScanReceiver, intentFilter)
+    }
+
+    fun startScan(): Boolean {
+        if (checkLocationPermission()) {
+            wifiManager.startScan()
+            return true
+        } else {
+            return false
+        }
+    }
+    fun toggleWifi() {
+        // Disattiva il Wi-Fi
+        wifiManager.isWifiEnabled = false
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Riattiva il Wi-Fi
+            wifiManager.isWifiEnabled = true
+        }, 1000) // Attendi un secondo prima di riattivare il Wi-Fi
+    }
+
+    fun startScanDelayed() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Avvia la scansione dopo che il Wi-Fi è stato riabilitato
+            if (checkLocationPermission()) {
+                wifiManager.startScan()
+            }
+        }, 1000) // Attendi un secondo dopo che il Wi-Fi è stato riattivato
+    }
+    private fun checkLocationPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun unregisterReceiver() {
+        context.unregisterReceiver(wifiScanReceiver)
+    }
 }
