20602f471ca7ef6a14d4727847f78d7456e55f99
Giacomo Moro <134197604+GiacomoMoro@users.noreply.github.com>
2024-04-28 18:03:02 +0200

    Retrofit library Import

---
+++ Fingerprinting Application/app/src/main/java/it/unipi/dii/fingerprintingapplication/ApiService.kt
@@ -0,0 +1,21 @@
+package it.unipi.dii.fingerprintingapplication
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ApiService {
+    @POST("maps")
+    fun createMap(@Body mapInfo: MapInfo): Call<MapResponse>
+}
+
+data class MapInfo(
+    val rooms: Int,
+    val buildingName: String,
+    val mapId: String
+)
+
+data class MapResponse(
+    val status: String,
+    val message: String?
+)
